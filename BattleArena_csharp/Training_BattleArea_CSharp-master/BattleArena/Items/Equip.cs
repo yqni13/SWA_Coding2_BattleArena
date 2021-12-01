2dfdcbc265f5fdae303c96745ab949d50af8247a
yqni13 <65848806+yqni13@users.noreply.github.com>
2021-12-01 23:32:40 +0100

    random choice of weapons for players

---
+++ BattleArena_csharp/Training_BattleArea_CSharp-master/BattleArena/Items/Equip.cs
@@ -0,0 +1,29 @@
+﻿using BattleArena.Items.OldVersion;
+using System;
+
+namespace BattleArena.Items
+{
+    class Equip
+    {
+        public object IEquipment { get; private set; }
+
+        public enum EquipmentItem
+        {
+            LatharSword = 0,
+            CynradBow = 1
+        }
+
+        public IEquipment EquipHero(Random randomNumberGenerator)
+        {
+            switch (randomNumberGenerator.Next(2))
+            {
+                case (0):
+                     return new CynradBow(randomNumberGenerator);
+                case (1):
+                    return new ObjectAdapterLatharSword(new Items.OldVersion.LatharSword(randomNumberGenerator));
+                default:
+                    return new CynradBow(randomNumberGenerator);
+            }
+        }
+    }
+}
